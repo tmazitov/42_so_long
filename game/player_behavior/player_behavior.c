eb5a601437f7c5dc88ec0212c79e81c0b9fe1cab
tmazitov <tlenovo@lenovo>
2023-12-07 16:00:10 +0400

    add the exit func

--- game/player_behavior/player_behavior.c
+++ game/player_behavior/player_behavior.c
@@ -6,11 +6,13 @@
 /*   By: tmazitov <tmazitov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 19:58:47 by tmazitov          #+#    #+#             */
-/*   Updated: 2023/12/07 14:09:15 by tmazitov         ###   ########.fr       */
+/*   Updated: 2023/12/07 15:59:34 by tmazitov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "player_behavior.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void	player_attack_handler(t_player *player)
 {
@@ -26,7 +28,7 @@ void	player_attack_handler(t_player *player)
 		add_task(player, ATTACK_1, player->anime->attack_left, 0);
 }
 
-void	player_open_chest_handler(t_game *game)
+int	player_open_chest_handler(t_game *game)
 {
 	int			counter;
 	int			chest_money;
@@ -35,6 +37,8 @@ void	player_open_chest_handler(t_game *game)
 
 	player = game->player;
 	counter = 0;
+	chest_money = 0;
+	chest = NULL;
 	while (game->scene->chests[counter])
 	{
 		chest = game->scene->chests[counter];
@@ -43,6 +47,28 @@ void	player_open_chest_handler(t_game *game)
 		counter++;
 	}
 	game->player->money += chest_money;
+	return (chest_money);
+}
+
+void	player_exit_handler(t_game *game)
+{
+	t_collider	*exit_coll;
+	t_collider	*player_coll;
+
+	player_coll = game->player->coll;
+	exit_coll = game->scene->exit->obj->coll;
+	if (check_intersection(player_coll, exit_coll, OPEN_CHEST, 0))
+	{
+		free_game(game);
+		exit(EXIT_SUCCESS);
+	}
+}
+
+void	shift_handler(t_game *game)
+{
+	if (player_open_chest_handler(game))
+		return ;
+	player_exit_handler(game);
 }
 
 int	player_control_hook(int keycode, t_game *game)
@@ -62,6 +88,6 @@ int	player_control_hook(int keycode, t_game *game)
 	if (keycode == BUTTON_SPACE)
 		player_attack_handler(player);
 	if (keycode == BUTTON_SHIFT)
-		player_open_chest_handler(game);
+		shift_handler(game);
 	return 0;
 }
