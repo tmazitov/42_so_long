6d95f67d537003c3502f128ddf5ad3b97cff76c2
Timur Mazitov <tmazitov@lab1r3s2.42abudhabi.ae>
2024-03-13 17:06:30 +0400

    bugfix: wrong type of the exit_game function

--- cmd/hooks.c
+++ cmd/hooks.c
@@ -6,16 +6,17 @@
 /*   By: tmazitov <tmazitov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 22:46:43 by tmazitov          #+#    #+#             */
-/*   Updated: 2024/03/13 16:51:26 by tmazitov         ###   ########.fr       */
+/*   Updated: 2024/03/13 17:05:46 by tmazitov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
-static void	exit_game(t_game *game)
+static int	exit_game(t_game *game)
 {
 	free_game(game);
 	exit(EXIT_SUCCESS);
+	return (0);
 }
 
 void	setup_hooks(t_game *game)
